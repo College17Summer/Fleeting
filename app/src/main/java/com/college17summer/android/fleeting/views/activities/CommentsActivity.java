26b3a5dd9ce5faf76ffd120995b1a830df16fb94
Moon Monkey <664102903@qq.com>
2017-06-26 09:56:36 +0800

    Add some activity.

---
+++ app/src/main/java/com/college17summer/android/fleeting/views/activities/CommentsActivity.java
@@ -0,0 +1,15 @@
+package com.college17summer.android.fleeting.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.college17summer.android.fleeting.R;
+
+public class CommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+    }
+}
