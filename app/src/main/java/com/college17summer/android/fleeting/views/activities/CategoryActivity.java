b5349affdd0d4fb1a09f4fd78c6188c32c0716c8
Moon Monkey <664102903@qq.com>
2017-06-26 09:23:15 +0800

    Add some activity.

---
+++ app/src/main/java/com/college17summer/android/fleeting/views/activities/CategoryActivity.java
@@ -0,0 +1,15 @@
+package com.college17summer.android.fleeting.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.college17summer.android.fleeting.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
